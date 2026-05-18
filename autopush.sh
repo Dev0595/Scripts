82d437c5f955c2183838eba8bbf72074fccf17de
feera <twork8808@gmail.com>
2026-05-18 13:14:40 +0100

    successful push

--- autopush.sh
+++ autopush.sh
@@ -3,8 +3,8 @@
 #Author:Arif
 
 cd /home/feera/scripts
-usr/bin/git add .
-usr/bin/git commit -m "Auto Push: $(date: '+%Y-%m-%d %H:%M:%S')"
-usr/bin/git push origin feera
+/usr/bin/git add .
+/usr/bin/git commit -m "Auto Push: $(date '+%Y-%m-%d %H:%M:%S')"
+/usr/bin/git push origin feera
 
 
